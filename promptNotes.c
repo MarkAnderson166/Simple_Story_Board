79e7d5481e437c9ba8e52556cfc7d9d827602763
Mark Anderson <mander53@myune.edu.au>
2023-04-29 17:43:45 +1000

    done, but free ai tools can't do this ... yet

---
+++ promptNotes.c
@@ -0,0 +1,66 @@
+/*
+
+User
+Write a brief narrator heavy script for a D&D scene where 4 adventurers meet at a bar before setting off on a bounty hunting contract.
+
+
+Dialog:
+
+The camera pans across a crowded medieval tavern, filled with rowdy patrons and the smell of stale ale. Suddenly, it focuses on a table in the corner, where four adventurers have just arrived.
+
+The first is a tall, muscular half-orc, with a scar running down his left cheek and a battle-axe slung over his shoulder. He wears a suit of chain mail, and a look of fierce determination on his face.
+
+Next to him sits a slender elf, with a bow slung over her shoulder and a quiver of arrows at her side. She wears a green cloak and a hood pulled over her head, and her sharp eyes scan the room for potential threats.
+
+Across from them sits a portly dwarf, with a bushy brown beard and a leather apron covering his sturdy frame. He carries a large hammer at his side, and his eyes sparkle with the promise of riches and adventure.
+
+Last but not least is a human wizard, with a pointed hat and a long robe that trails across the floor. He carries a staff in one hand and a spellbook in the other, and his eyes flicker with the power of arcane magic.
+
+As they settle into their seats, a grizzled old barkeep approaches them. "You folks lookin' for work?" he asks, eyeing their weapons and armor. "I got a line on a bounty, if you're interested."
+
+The adventurers exchange a glance, their eyes gleaming with excitement. "Tell us more," the half-orc rumbles.
+
+The barkeep leans in, his voice low and conspiratorial. "There's a band of goblins been raiding nearby villages, stealing food and livestock. The mayor's offered a reward for anyone who brings 'em in."
+
+The dwarf grins, his eyes alight with greed. "How much we talkin'?"
+
+The barkeep names a figure, and the adventurers nod in agreement. "We're in," the elf declares.
+
+The camera fades to black as the four adventurers clink their mugs together, eager to set off on their next adventure.
+
+
+
+Images:    (https://beta.dreamstudio.ai/generate) -(stable diffusion)
+
+0 -> 7
+
+four adventurers have just arrived at a medieval tavern.
+
+The first is a tall, muscular half-orc, with a scar running down his left cheek and a battle-axe slung over his shoulder. He wears a suit of chain mail, and a look of fierce determination on his face.
+
+Next to him sits a slender elf, with a bow slung over her shoulder and a quiver of arrows at her side. She wears a green cloak and a hood pulled over her head, and her sharp eyes scan the room for potential threats.
+
+Across from them sits a portly dwarf, with a bushy brown beard and a leather apron covering his sturdy frame. He carries a large hammer at his side, and his eyes sparkle with the promise of riches and adventure.
+
+Last but not least is a human wizard, with a pointed hat and a long robe that trails across the floor. He carries a staff in one hand and a spellbook in the other, and his eyes flicker with the power of arcane magic.
+
+
+8 -> 
+
+four adventurers settle into their seats at a medieval tavern.
+, a grizzled old barkeep approaches them. "You folks lookin' for work?" he asks, eyeing their weapons and armor. "I got a line on a bounty, if you're interested."
+
+The adventurers exchange a glance, their eyes gleaming with excitement. "Tell us more," the half-orc rumbles.
+
+The barkeep leans in, his voice low and conspiratorial. "There's a band of goblins been raiding nearby villages, stealing food and livestock. The mayor's offered a reward for anyone who brings 'em in."
+
+The dwarf grins, his eyes alight with greed. "How much we talkin'?"
+
+The barkeep names a figure, and the adventurers nod in agreement. "We're in," the elf declares.
+
+The camera fades to black as the four adventurers clink their mugs together, eager to set off on their next adventure.
+
+
+
+
+*/
