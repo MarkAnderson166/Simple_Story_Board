5687e2e8c2c0196397a6ed2097f5847079c34646
Mark Anderson <mander53@myune.edu.au>
2023-06-01 15:00:34 +1000

    rawgpt add

--- rawGPT_because_i_forgot_my_password.py
+++ rawGPT_because_i_forgot_my_password.py
@@ -3,15 +3,16 @@ import openai
 # pip install openai  
 
 openai.api_key = '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' # 'YOUR_API_KEY'
+openai.api_key = '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'
 # name?: My_Test_Key_For_Python_API_Test
 
-messages_dave = [ {"role": "system", "content": "software developer."} ]
+messages_dave = [ {"role": "system", "content": "helpfull assistant"} ]
 
 while True:
 
   message = "null"
 
-  while message is not "x" and message is not "exit":
+  while message != "x" and message != "exit":
     message = input("input : ")
     messages_dave.append({"role": "user", "content": message},)
     chat = openai.ChatCompletion.create( model="gpt-3.5-turbo", messages=messages_dave )
@@ -19,3 +20,4 @@ while True:
     print(f" \n{reply}\n")
     messages_dave.append({"role": "assistant", "content": reply})
 
+# come up with an original name for a weapon in an rpg game, in precisly this format: "Name - Some flavour text, no more than 15 words" this one is a sword that boosts intelligence.
